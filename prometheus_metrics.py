b6695e27063e7ae111e996777065dd06ad2680ad
Nicole Bilaw <nicolegbilaw@gmail.com>
2023-08-29 22:44:05 +0800

    Add collateral and upnl_pct metrics in prometheus

--- prometheus_metrics.py
+++ prometheus_metrics.py
@@ -1,6 +1,24 @@
 from prometheus_client import Gauge
 
-metrics = {}
-metrics['mint_gauge'] = Gauge('ovl_token_minted', 'Number of OVL tokens minted', ['market'])
-metrics['upnl_gauge'] = Gauge('upnl', 'Unrealised profit and loss', ['market'])
-metrics['upnl_pct_gauge'] = Gauge('upnl_pct', 'Unrealised profit and loss (percent)', ['market'])
+metrics = {
+   'mint_gauge': Gauge(
+        'ovl_token_minted',
+        'Number of OVL tokens minted',
+        ['market']
+    ),
+   'upnl_gauge': Gauge(
+        'upnl',
+        'Unrealised profit and loss',
+        ['market']
+    ),
+   'collateral_rem_gauge': Gauge(
+        'collateral_rem',
+        'Collateral',
+        ['market']
+    ),
+   'upnl_pct_gauge': Gauge(
+        'upnl_pct',
+        'Unrealised profit and loss (Percentage)',
+        ['market']
+    ),
+}
