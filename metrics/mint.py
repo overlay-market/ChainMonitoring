ea62685a6621d46373e6b55902b6b7fc18224354
Nicole Bilaw <nicolegbilaw@gmail.com>
2023-09-01 00:31:48 +0800

    Wrap metric queries in try-catch

--- metrics/mint.py
+++ metrics/mint.py
@@ -3,60 +3,94 @@ import math
 import threading
 import time
 
+from constants import MAP_MARKET_ID_TO_NAME, AVAILABLE_MARKETS, ALL_MARKET_LABEL
 from prometheus_metrics import metrics
 from subgraph.client import ResourceClient as SubgraphClient
-from subgraph.constants import MAP_MARKET_ID_TO_NAME
+
+
+subgraph_client = SubgraphClient()
+
+
+def set_metrics_to_nan():
+    # Set metric to NaN to indicate that something went wrong with the query
+    metrics['mint_gauge'].labels(market=ALL_MARKET_LABEL).set(0)
+    for market in AVAILABLE_MARKETS:
+        metrics['mint_gauge'].labels(market=MAP_MARKET_ID_TO_NAME[market]).set(0)
 
 
 def query_mint():
     print('[ovl_token_minted] Starting query...')
-    subgraph_client = SubgraphClient()
-    iteration = 1
-    query_interval = 10 # in seconds
-    timestamp_window = 20 # in seconds
-    mint_divisor = 10 ** 18
+    try:
+        iteration = 1
+        query_interval = 10 # in seconds
+        timestamp_window = 20 # in seconds
+        mint_divisor = 10 ** 18
 
-    # Calculate the total mint so far from the subgraph
-    mint_total, mint_total_per_market = subgraph_client.get_mint_total_per_market()
-    metrics['mint_gauge'].labels(market='').set(mint_total / mint_divisor)
-    for market_id in mint_total_per_market:
-        metrics['mint_gauge'].labels(market=MAP_MARKET_ID_TO_NAME[market_id]).set(mint_total_per_market[market_id] / mint_divisor)
+        # Calculate the total mint so far from the subgraph
+        mint_total, mint_total_per_market = subgraph_client.get_mint_total_per_market()
+        metrics['mint_gauge'].labels(market=ALL_MARKET_LABEL).set(mint_total / mint_divisor)
+        for market_id in mint_total_per_market:
+            if market_id not in AVAILABLE_MARKETS:
+                continue
+            metrics['mint_gauge'].labels(market=MAP_MARKET_ID_TO_NAME[market_id]).set(mint_total_per_market[market_id] / mint_divisor)
 
-    # the first ever timestamp for positions is 1677258816
-    timestamp_start = datetime.datetime.now().timestamp() - (timestamp_window)
-    timestamp_lower = math.ceil(timestamp_start - timestamp_window)
-    timestamp_upper = math.ceil(timestamp_start)
+        # the first ever timestamp for positions is 1677258816
+        timestamp_start = datetime.datetime.now().timestamp() - (timestamp_window)
+        timestamp_lower = math.ceil(timestamp_start - timestamp_window)
+        timestamp_upper = math.ceil(timestamp_start)
 
-    while True:
-        print('===================================')
-        print(f'[ovl_token_minted] Running iteration #{iteration}...')
-        print('[ovl_token_minted] timestamp_lower', datetime.datetime.utcfromtimestamp(timestamp_lower).strftime('%Y-%m-%d %H:%M:%S'))
-        print('[ovl_token_minted] timestamp_upper', datetime.datetime.utcfromtimestamp(timestamp_upper).strftime('%Y-%m-%d %H:%M:%S'))
-        
-        # Update ovl_token_minted metric
-        positions = subgraph_client.get_positions(timestamp_lower, timestamp_upper)
-        print(f'[ovl_token_minted] positions', len(positions))
-        for position in positions:
-            mint = int(position['mint']) / mint_divisor
-            metrics['mint_gauge'].labels(market=MAP_MARKET_ID_TO_NAME[position['market']['id']]).inc(mint)
-            metrics['mint_gauge'].labels(market='').inc(mint)
+        while True:
+            try:
+                print('===================================')
+                print(f'[ovl_token_minted] Running iteration #{iteration}...')
+                print(
+                    '[ovl_token_minted] timestamp_lower',
+                    datetime.datetime.utcfromtimestamp(timestamp_lower).strftime('%Y-%m-%d %H:%M:%S')
+                )
+                print(
+                    '[ovl_token_minted] timestamp_upper',
+                    datetime.datetime.utcfromtimestamp(timestamp_upper).strftime('%Y-%m-%d %H:%M:%S')
+                )
+                
+                # Update ovl_token_minted metric
+                positions = subgraph_client.get_positions(timestamp_lower, timestamp_upper)
+                positions = [
+                    position
+                    for position in positions
+                    if position['market']['id'] in AVAILABLE_MARKETS
+                ]
+                print(f'[ovl_token_minted] positions', len(positions))
+                for position in positions:
+                    mint = int(position['mint']) / mint_divisor
+                    market = MAP_MARKET_ID_TO_NAME[position['market']['id']]
+                    metrics['mint_gauge'].labels(market=market).inc(mint)
+                    metrics['mint_gauge'].labels(market=ALL_MARKET_LABEL).inc(mint)
 
-        # Increment iteration
-        iteration += 1
+                # Increment iteration
+                iteration += 1
 
-        # Wait for the next iteration
-        time.sleep(query_interval)
+                # Wait for the next iteration
+                time.sleep(query_interval)
 
-        if positions:
-            # set timestamp range lower bound to timestamp of latest event
-            timestamp_lower = int(positions[0]['createdAtTimestamp'])
-            if len(positions) > 1:
-                timestamp_upper = int(positions[-1]['createdAtTimestamp'])
-            else:
-                timestamp_upper += timestamp_window
-        else:
-            timestamp_lower = timestamp_upper
-            timestamp_upper += timestamp_window
+                if positions:
+                    # set timestamp range lower bound to timestamp of latest event
+                    timestamp_lower = int(positions[0]['createdAtTimestamp'])
+                    if len(positions) > 1:
+                        timestamp_upper = int(positions[-1]['createdAtTimestamp'])
+                    else:
+                        timestamp_upper += timestamp_window
+                else:
+                    timestamp_lower = timestamp_upper
+                    timestamp_upper += timestamp_window
+            except Exception as e:
+                print(
+                    f"[ovl_token_minted] An error occurred on iteration "
+                    f"{iteration} timestamp_lower "
+                    f"{datetime.datetime.utcfromtimestamp(timestamp_lower).strftime('%Y-%m-%d %H:%M:%S')}:", e)
+                set_metrics_to_nan()
+    except Exception as e:
+        print(f"[ovl_token_minted] An error occurred:", e)
+        set_metrics_to_nan()
 
 
 thread = threading.Thread(target=query_mint)
--- metrics/upnl.py
+++ metrics/upnl.py
@@ -9,9 +9,10 @@ from brownie import Contract, web3, network
 from dank_mids.brownie_patch import patch_contract
 from dank_mids.helpers import setup_dank_w3_from_sync
 
+from constants import MAP_MARKET_ID_TO_NAME as MARKET_MAP, AVAILABLE_MARKETS, ALL_MARKET_LABEL
 from prometheus_metrics import metrics
 from subgraph.client import ResourceClient as SubgraphClient
-from subgraph.constants import MAP_MARKET_ID_TO_NAME as MARKET_MAP
+
 
 network.connect('arbitrum-main')
 
@@ -22,26 +23,6 @@ dank_w3 = setup_dank_w3_from_sync(web3)
 STATE = '0xC3cB99652111e7828f38544E3e94c714D8F9a51a'
 
 subgraph_client = SubgraphClient()
-
-
-AVAILABLE_MARKETS = [
-  '0x02e5938904014901c96f534b063ec732ea3b48d5',
-  '0x1067b7df86552a53d816ce3fed50d6d01310b48f',
-  '0x33659282d39e62b62060c3f9fb2230e97db15f1e',
-  # '0x35e1d28ad9d8a80cff5bbf163a735c54eb6c1342', # 'AZUKI / WETH'
-  '0x5114215415ee91ab5d973ba62fa9153ece1f6c5a',
-  '0x7c65c99ba1edfc94c535b7aa2d72b0f7357a676b',
-  '0x833ba1a942dc6d33bc3e6959637ae00e0cdcb20b',
-  # '0x8c7dc90243fc7984583339da8df0a5d57ec491db', # 'PUDGIES / WETH'
-  # '0x8c82c349e349ffd9403c3984cb1ad1b0f76f7d2e', # 'PUNKS / WETH'
-  '0xa811698d855153cc7472d1fb356149a94bd618e7',
-  # '0xb31d222c23104cbc2c04df77941f1f2c478133dd', # 'BAYC / WETH'
-  '0xc28350047d006ed387b0f210d4ea3218137a8a38',
-  # '0xccd645835ca0033f0c1106e7b24f288e59e867e8', # 'MILADY / WETH'
-  # '0xce45c64911bd0a088daabd73ee1bc09ae98cd84b', # 'MAYC / WETH'
-  # '0xF30C5cB6205f115799b275430Ea0874359476304'.toLowerCase(), # Total Crypto Market Cap
-  # '0x8440E56C2675d9b8E04183dA3a3a744a4a16ED33'.toLowerCase(), # memecoins
-]
 
 
 def load_contract(address):
@@ -93,98 +74,122 @@ async def process_live_positions(live_positions):
     return live_positions_df
 
 
+def set_metrics_to_nan():
+    # Set metric to NaN to indicate that something went wrong with the query
+    metrics['upnl_gauge'].labels(market=ALL_MARKET_LABEL).set(math.nan)
+    metrics['collateral_rem_gauge'].labels(market=ALL_MARKET_LABEL).set(math.nan)
+    metrics['upnl_pct_gauge'].labels(market=ALL_MARKET_LABEL).set(math.nan)
+    for market in AVAILABLE_MARKETS:
+        metrics['upnl_gauge'].labels(market=MARKET_MAP[market]).set(math.nan)
+        metrics['collateral_rem_gauge'].labels(market=MARKET_MAP[market]).set(math.nan)
+        metrics['upnl_pct_gauge'].labels(market=MARKET_MAP[market]).set(math.nan)
+
+
 async def query_upnl():
     print('[upnl] Starting query...')
-    iteration = 1
-    query_interval = 10 # in seconds
-    timestamp_window = 10 # in seconds
-    timestamp_start = datetime.datetime.now().timestamp()
+    try:
+        iteration = 1
+        query_interval = 10 # in seconds
+        timestamp_window = 10 # in seconds
+        timestamp_start = datetime.datetime.now().timestamp()
 
-    # Fetch all live positions so far from the subgraph
-    live_positions = subgraph_client.get_all_live_positions()
-
-    # Calculate current value of each live position
-    live_positions_df = await process_live_positions(live_positions)
-
-    # Set initial value of upnl metric so far
-    upnl_total = live_positions_df['upnl'].sum()
-    upnl_total_per_market_df = live_positions_df.groupby(by='market')['upnl'].sum().reset_index()
-    upnl_total_per_market = dict(zip(upnl_total_per_market_df['market'], upnl_total_per_market_df['upnl']))
-    metrics['upnl_gauge'].labels(market='').set(upnl_total)
-    for market_id in upnl_total_per_market:
-        metrics['upnl_gauge'].labels(market=MARKET_MAP[market_id]).set(upnl_total_per_market[market_id])
-    print('upnl_total!!', upnl_total)
-    print('upnl_total_per_market!!', upnl_total_per_market)
-
-    # Set initial value for collateral metric so far
-    collateral_total = live_positions_df['collateral_rem'].sum()
-    collateral_total_per_market_df = live_positions_df.groupby(by='market')['collateral_rem'].sum().reset_index()
-    collateral_total_per_market = dict(zip(collateral_total_per_market_df['market'], collateral_total_per_market_df['collateral_rem']))
-    print('collateral_total!!', collateral_total)
-    print('collateral_total_per_market', collateral_total_per_market)
-    metrics['collateral_rem_gauge'].labels(market='').set(collateral_total)
-    for market_id in collateral_total_per_market:
-        metrics['collateral_rem_gauge'].labels(market=MARKET_MAP[market_id]).set(collateral_total_per_market[market_id])
-        metrics['upnl_pct_gauge'].labels(market=MARKET_MAP[market_id]).set(
-            upnl_total_per_market[market_id] / collateral_total_per_market[market_id]
-        )
-
-    # live_positions_df['upnl_pct'] = live_positions_df['upnl'] / live_positions_df['collateral_rem']
-    metrics['upnl_pct_gauge'].labels(market='').set(upnl_total / collateral_total)
-
-    timestamp_lower = math.ceil(timestamp_start)
-    timestamp_upper = math.ceil(timestamp_start + timestamp_window)
-
-    while True:
-        print('===================================')
-        print(f'[upnl] Running iteration #{iteration}...')
-        print('[upnl] timestamp_lower', datetime.datetime.utcfromtimestamp(timestamp_lower).strftime('%Y-%m-%d %H:%M:%S'))
-        print('[upnl] timestamp_upper', datetime.datetime.utcfromtimestamp(timestamp_upper).strftime('%Y-%m-%d %H:%M:%S'))
-
-        # Fetch new live positions
-        live_positions = subgraph_client.get_live_positions(timestamp_lower, timestamp_upper)
-        print(f'[upnl] live_positions', len(live_positions))
+        # Fetch all live positions so far from the subgraph
+        live_positions = subgraph_client.get_all_live_positions()
 
         # Calculate current value of each live position
-        if len(live_positions) > 0:
-            live_positions_df = await process_live_positions(live_positions)
-            for position in live_positions_df.to_dict(orient='records'):
-                market = MARKET_MAP[position['market']]
-                metrics['upnl_gauge'].labels(market=market).inc(position['upnl'])
-                metrics['upnl_gauge'].labels(market='').inc(position['upnl'])
+        live_positions_df = await process_live_positions(live_positions)
 
-                metrics['collateral_rem_gauge'].labels(market=market).inc(position['collateral_rem'])
-                metrics['collateral_rem_gauge'].labels(market='').inc(position['collateral_rem'])
-                
-                # live_positions_df['upnl_pct'] = live_positions_df['upnl'] / live_positions_df['collateral_rem']
-                # ._value.get()
-                metrics['upnl_pct_gauge'].labels(market=market).set(
-                    metrics['upnl_gauge'].labels(market=market)._value.get()
-                    / metrics['collateral_rem_gauge'].labels(market=market)._value.get()
-                )
-                metrics['upnl_pct_gauge'].labels(market='').set(
-                    metrics['upnl_gauge'].labels(market='')._value.get()
-                    / metrics['collateral_rem_gauge'].labels(market='')._value.get()
-                )
+        # Set initial value of upnl metric so far
+        upnl_total = live_positions_df['upnl'].sum()
+        upnl_total_per_market_df = live_positions_df.groupby(by='market')['upnl'].sum().reset_index()
+        upnl_total_per_market = dict(zip(upnl_total_per_market_df['market'], upnl_total_per_market_df['upnl']))
+        metrics['upnl_gauge'].labels(market=ALL_MARKET_LABEL).set(upnl_total)
+        for market_id in upnl_total_per_market:
+            metrics['upnl_gauge'].labels(market=MARKET_MAP[market_id]).set(upnl_total_per_market[market_id])
+        print('[upnl] upnl_total!!', upnl_total)
+        print('[upnl] upnl_total_per_market!!', upnl_total_per_market)
 
-        # Increment iteration
-        iteration += 1
+        # Set initial value for collateral metric so far
+        collateral_total = live_positions_df['collateral_rem'].sum()
+        collateral_total_per_market_df = live_positions_df.groupby(by='market')['collateral_rem'].sum().reset_index()
+        collateral_total_per_market = dict(zip(collateral_total_per_market_df['market'], collateral_total_per_market_df['collateral_rem']))
+        print('[upnl] collateral_total!!', collateral_total)
+        print('[upnl] collateral_total_per_market', collateral_total_per_market)
+        metrics['collateral_rem_gauge'].labels(market=ALL_MARKET_LABEL).set(collateral_total)
+        for market_id in collateral_total_per_market:
+            metrics['collateral_rem_gauge'].labels(market=MARKET_MAP[market_id]).set(collateral_total_per_market[market_id])
+            metrics['upnl_pct_gauge'].labels(market=MARKET_MAP[market_id]).set(
+                upnl_total_per_market[market_id] / collateral_total_per_market[market_id]
+            )
 
-        # Wait for the next iteration
-        await asyncio.sleep(query_interval)
+        # live_positions_df['upnl_pct'] = live_positions_df['upnl'] / live_positions_df['collateral_rem']
+        metrics['upnl_pct_gauge'].labels(market=ALL_MARKET_LABEL).set(upnl_total / collateral_total)
 
-        if len(live_positions) > 0:
-            # set timestamp range lower bound to timestamp of latest event
-            timestamp_lower = int(live_positions[0]['timestamp'])
-            if len(live_positions) > 1:
-                timestamp_upper = int(live_positions[-1]['timestamp'])
-            else:
-                timestamp_upper += timestamp_window
-        else:
-            timestamp_lower = timestamp_upper
-            timestamp_upper += timestamp_window
+        timestamp_lower = math.ceil(timestamp_start)
+        timestamp_upper = math.ceil(timestamp_start + timestamp_window)
+
+        while True:
+            try:
+                print('===================================')
+                print(f'[upnl] Running iteration #{iteration}...')
+                print('[upnl] timestamp_lower', datetime.datetime.utcfromtimestamp(timestamp_lower).strftime('%Y-%m-%d %H:%M:%S'))
+                print('[upnl] timestamp_upper', datetime.datetime.utcfromtimestamp(timestamp_upper).strftime('%Y-%m-%d %H:%M:%S'))
+
+                # Fetch new live positions
+                live_positions = subgraph_client.get_live_positions(timestamp_lower, timestamp_upper)
+                print(f'[upnl] live_positions', len(live_positions))
+
+                # Calculate current value of each live position
+                if len(live_positions) > 0:
+                    live_positions_df = await process_live_positions(live_positions)
+                    for position in live_positions_df.to_dict(orient='records'):
+                        market = MARKET_MAP[position['market']]
+                        metrics['upnl_gauge'].labels(market=market).inc(position['upnl'])
+                        metrics['upnl_gauge'].labels(market=ALL_MARKET_LABEL).inc(position['upnl'])
+
+                        metrics['collateral_rem_gauge'].labels(market=market).inc(position['collateral_rem'])
+                        metrics['collateral_rem_gauge'].labels(market=ALL_MARKET_LABEL).inc(position['collateral_rem'])
+                        
+                        # live_positions_df['upnl_pct'] = live_positions_df['upnl'] / live_positions_df['collateral_rem']
+                        # ._value.get()
+                        metrics['upnl_pct_gauge'].labels(market=market).set(
+                            metrics['upnl_gauge'].labels(market=market)._value.get()
+                            / metrics['collateral_rem_gauge'].labels(market=market)._value.get()
+                        )
+                        metrics['upnl_pct_gauge'].labels(market=ALL_MARKET_LABEL).set(
+                            metrics['upnl_gauge'].labels(market=ALL_MARKET_LABEL)._value.get()
+                            / metrics['collateral_rem_gauge'].labels(market=ALL_MARKET_LABEL)._value.get()
+                        )
+
+                # Increment iteration
+                iteration += 1
+
+                # Wait for the next iteration
+                await asyncio.sleep(query_interval)
+
+                if len(live_positions) > 0:
+                    # set timestamp range lower bound to timestamp of latest event
+                    timestamp_lower = int(live_positions[0]['timestamp'])
+                    if len(live_positions) > 1:
+                        timestamp_upper = int(live_positions[-1]['timestamp'])
+                    else:
+                        timestamp_upper += timestamp_window
+                else:
+                    timestamp_lower = timestamp_upper
+                    timestamp_upper += timestamp_window
+            except Exception as e:
+                print(
+                    f"[upnl] An error occurred on iteration "
+                    f"{iteration} timestamp_lower "
+                    f"{datetime.datetime.utcfromtimestamp(timestamp_lower).strftime('%Y-%m-%d %H:%M:%S')}:", e)
+                set_metrics_to_nan()
+    except Exception as e:
+        print(f"[upnl] An error occurred:", e)
+        set_metrics_to_nan()
+
 
 thread = threading.Thread(target=asyncio.run, args=(query_upnl(),))
+
 
 if __name__ == '__main__':
     asyncio.run(query_upnl)
