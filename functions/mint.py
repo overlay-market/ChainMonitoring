0dc77d49b3c2d21dbfc40c676865974838cf99cb
Nicole Bilaw <nicolegbilaw@gmail.com>
2024-01-06 03:31:46 +0800

    Remove commented code

--- functions/mint.py
+++ functions/mint.py
@@ -88,9 +88,6 @@ class Handler(BaseMonitoringHandler):
         calculated_metrics = self.calculate_metrics()
         print('calculated_metrics!!', calculated_metrics)
 
-        # [{'name': 'ovl_token_minted', 'value': {'ALL': -805.7973282971279, 'LINK / USD': -94.96272053405455, 'SOL / USD': -167.26353028247604, 'APE / USD': -69.12437849497073, 'Crypto Volatility Index': 240.00793524473616, 'AVAX / USD': -84.59636423287223, 'MATIC / USD': -100.97709948693603, 'WBTC / USD': -528.8811705105545}}]
-
-
         # calculated_metrics = {
         #     'ovl_token_minted': {
         #         'ALL': -805.7973282971279,
