4ad67915e3ce1b5fa4c838c7b43511b7cb44660e
Nicole Bilaw <nicolegbilaw@gmail.com>
2024-01-06 03:49:01 +0800

    Send out alert from mint handler

--- base/handler.py
+++ base/handler.py
@@ -52,10 +52,42 @@ class BaseMonitoringHandler:
         }
 
     def alert(self):
-        # Define alert logic here (green, orange, red)
-        # self.calculate_metrics()
-        # Send alerts for green, orange, red
-        raise NotImplementedError
+        # Calculate metrics
+        calculated_metrics = self.calculate_metrics()
+        print('calculated_metrics!!', calculated_metrics)
+
+        # calculated_metrics = {
+        #     'ovl_token_minted': {
+        #         'ALL': -805.7973282971279,
+        #         'LINK / USD': -94.96272053405455,
+        #         'SOL / USD': -167.26353028247604,
+        #         'APE / USD': -69.12437849497073,
+        #         'Crypto Volatility Index': 240.00793524473616,
+        #         'AVAX / USD': -84.59636423287223,
+        #         'MATIC / USD': -100.97709948693603,
+        #         'WBTC / USD': -528.8811705105545
+        #     }
+        # }
+
+        for alert_level, rule in self.alert_rules.items():
+            for rule_name, rule_func in rule.items():
+                for metric_name, metric in calculated_metrics.items():
+                    for metric_label, metric_value in metric.items():
+                        print('alert_level', alert_level)
+                        print('rule_name', rule_name)
+                        print('metric_name', metric_name)
+                        print('metric_label', metric_label)
+                        should_alert = rule_func(metric_value)
+                        print('should_alert', should_alert)
+                        print('=================================')
+
+                        if should_alert:
+                            send_alert(
+                                alert_level,
+                                rule_name,
+                                metric_name,
+                                metric_label
+                            )
 
     def run(self):
         # TO-DO: run alert function every heartbeat sec
